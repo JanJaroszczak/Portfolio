6ea21c3d06b9bb12728e1c8210a6aa6c79aeb78f
Jan Jaroszczak <jan.jaroszczak@gmail.com>
2021-05-04 13:43:55 +0200

    IMPROVEMENT: HomePage styles moved to a separate folder.

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from '../components/organisms/Header';
 
-const StyledHomePageWrapper = styled.div`
-  height: 2000px;
-`;
+import { StyledHomePageWrapper } from './styles/StyledHomePage';
 
 const HomePage = () => {
   return (
---
+++ src/views/styles/StyledHomePage.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledHomePageWrapper = styled.div`
+  height: 2000px;
+`;
